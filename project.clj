c209fcd52b38ed1b88c054c56a226df3772e20b4
FerdiKuehne <ferdi.kuehne@gmx.net>
2019-03-14 10:47:17 +0100

    Refactor test env for cljs.

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
                  [com.taoensso/carmine "2.12.2"]
                  [org.clojure/core.rrb-vector "0.0.14"]
                  [org.clojure/core.cache "0.7.2"]
-                 [io.replikativ/konserve "0.5.0"]]
+                 [io.replikativ/konserve "0.5.1-SNAPSHOT"]
+                 [io.replikativ/incognito "0.2.5-SNAPSHOT"]]
   :aliases {"bench" ["with-profile" "profiling" "run" "-m" "hitchhiker.bench"]}
   :jvm-opts ["-server" "-Xmx3700m" "-Xms3700m"]
   :profiles {:test
@@ -25,35 +26,35 @@
              :dev {:dependencies [[binaryage/devtools "0.9.10"]
                                   [figwheel-sidecar "0.5.18"]
                                   [org.clojure/test.check "0.9.0"]
-                                  ;; plotting
                                   [aysylu/loom "1.0.2"]
                                   [cheshire "5.8.1"]]
                    :source-paths ["src" "dev"]
                    :plugins [[lein-figwheel "0.5.18"]]
                    :repl-options {; for nREPL dev you really need to limit output
                                   :init (set! *print-length* 50)}}}
+
   :clean-targets ^{:protect false} ["resources/public/js" "target"]
 
+  :hooks [leiningen.cljsbuild]
+
+  :source-paths ["src"]
+  :test-paths ["test"]
 
   :cljsbuild {:builds
-              [{:id "dev"
-                :figwheel true
+              {:dev
+               {:figwheel true
                 :source-paths ["src"]
                 :compiler {:main hitchhiker.tree.core
                            :asset-path "js/out"
                            :output-to "resources/public/js/core.js"
                            :output-dir "resources/public/js/out" }}
-               ;; inspired by datascript project.clj
-               {:id "test"
-                :source-paths ["src" "test"]
-                :compiler {
-                           :main          hitchhiker.konserve-test
-                           :output-to     "target/test.js"
-                           :output-dir    "target/none"
+               :tests
+               {:source-paths ["src" "test"]
+                :notify-command ["node" "target/unit-tests.js"]
+                :compiler {:output-to "target/unit-tests.js"
                            :optimizations :none
-                           :source-map    true
-                           :recompile-dependents false
-                           :parallel-build true}}]}
+                           :target :nodejs
+                           :main  hitchhiker.konserve-test}}}}
 
   :plugins [[lein-figwheel "0.5.18"]
-            [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]])
+            [lein-cljsbuild "1.1.7"]])
